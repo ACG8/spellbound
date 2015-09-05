d0e9a34d0f28600a585a6978daa286036fb47428
ACG <anandaguneratne@gmail.com>
2015-09-05 11:20:29 -0500

    Game now lists player's options for the next turn.
    Also changed the block graphics and restructured code using prototypes
    to improve game memory.

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 600;
+canvas.width = 800;
 canvas.height = 450;
 document.body.appendChild(canvas);
 
@@ -13,9 +13,9 @@ bgImage.onload = function () {
 };
 bgImage.src = "images/background.png";
 
-/////////////////////////////////////////////////////////////////////////
-///////////////////////////Preloading Images/////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////Preloading Images//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 var imageDict = {};
 
@@ -37,41 +37,78 @@ function loadImages() {
 
 loadImages();
 */
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////Game Settings///////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////Game Settings////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var roundTime = 5000, //Time of each round in ms
 
 	keybindings = { //Control bindings for players 1 and 2
 		P1: {
-			a: 87, //w
-			b: 69, //e
-			c: 65, //a
-			d: 83, //s
-			e: 68  //d
+			a:  81, //q
+			b:  87, //w
+			c:  69, //e
+			d:  65, //a
+			e:  68, //d
+			"": 83  //s
 		},
 		P2: {
-			a: 79, //o
-			b: 80, //p
-			c: 76, //l
-			d: 186,//;
-			e: 222 //'
+			a:  219, //[
+			b:  80, //p
+			c:  79, //o
+			d:  222,//'
+			e:  76, //l
+			"": 186 //;
+		}
+	},
+	runeUR = new Image(),
+	runeU = new Image(),
+	runeUL = new Image(),
+	runeL = new Image(),
+	runeR = new Image(),
+	runeS = new Image(),
+
+	runeBindings = {
+		P1: {
+			a:  runeUL,
+			b:  runeU,
+			c:  runeUR, 
+			d:  runeL,
+			e:  runeR,
+			"": runeS
+		},
+		P2: {
+			a:  runeUR,
+			b:  runeU,
+			c:  runeUL,
+			d:  runeR,
+			e:  runeL,
+			"": runeS
 		}
 	};
 
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////Constructor Functions///////////////////////////
-/////////////////////////////////////////////////////////////////////////
+runeUR.src = "images/URBlock.png";
+runeU.src = "images/UBlock.png";
+runeUL.src = "images/ULBlock.png";
+runeL.src = "images/LBlock.png";
+runeR.src = "images/RBlock.png";
+runeS.src = "images/SBlock.png";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////Constructor Functions////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function Actor(imagesrc,x,y) {
 	this.image = new Image();
-	this.image.src = "images/"+imagesrc+".png";
+	if (imagesrc) {this.image.src = "images/"+imagesrc+".png"};
 	//this.image = imageDict[imagesrc];
 	this.x = x;
 	this.y = y;
-	this.draw = function() {ctx.drawImage(this.image,this.x,this.y)};
-	this.isTouching = function(other) {
+	this.isCondemned = false;
+}
+
+Actor.prototype = {
+	draw : function() {ctx.drawImage(this.image,this.x,this.y)},
+	isTouching : function(other) {
 		var L1=this.x,
 			R1=L1+this.image.width,
 			U1=this.y,
@@ -81,17 +118,18 @@ function Actor(imagesrc,x,y) {
 			U2=other.y,
 			D2=U2+other.image.height;
 		return (!(L1>R2 || L2>R1 || U1>D2 || U2>D1));
-	};
-	this.isOffscreen = function() {
+	},
+	isOffscreen : function() {
 		var L=this.x,R=L+this.image.width,U=this.y,D=U+this.image.height;
 		return (L<0 || canvas.width<R || U<0 || canvas.height<D);
-	};
-	this.getTouching = function() {
+	},
+	getTouching : function() {
 		var combinedObjects = creatures + conjurations;
 		return combinedObjects.filter(function(o) {return (this.isTouching(o) && (o!==this))})
-	};
-	this.isCondemned = false;
+	}
 }
+
+//_____________________________Creature_________________________________________________________________________________________________________
 
 function Creature(level,health,speed,controller,imagesrc,x,y) {
 	Actor.call(this,imagesrc);
@@ -101,44 +139,89 @@ function Creature(level,health,speed,controller,imagesrc,x,y) {
 	this.controller = controller;
 }
 
+Creature.prototype = new Actor();
+
+//_____________________________Wizard__________________________________________________________________________________________________________
+
 function Wizard(level,health,speed,imagesrc,x,y) {
 	Creature.call(this,level,health,speed,this,imagesrc,x,y);
 	this.words = new Words();
 }
 
+Wizard.prototype = new Creature();
+Wizard.prototype.publishHistory = function() {
+		var reversedHistory = this.words.history.slice(0);
+		reversedHistory.reverse();
+		reversedHistory.forEach(function(o,i) {
+			ctx.drawImage(runeBindings["P1"][o],canvas.width/2-100-i*50,0);
+		})
+	};
+Wizard.prototype.publishOptions = function() {
+		var options = this.words.anticipate(),key,i=0;
+		for (key in options) {
+			if (options[key].name) {
+				ctx.drawImage(runeBindings["P1"][key],0,50+i*50)
+				ctx.fillStyle = "rgb(250, 250, 250)";
+				ctx.font = "30px Helvetica";
+				ctx.fillStyle = "black";
+				ctx.textBaseline = "top";
+				ctx.textAlign = "left";
+				ctx.fillText("" + options[key].name, 50, 50+i*50);
+				i++
+			}
+		}
+	};
+
 function Words() {
 	this.history = [];
-	this.getLast = function(n) {
-		var history = this.history, L=history.length, returnList = [];
+}
+
+Words.prototype = {
+	getLast : function(n,history) {
+		history = history || this.history;
+		var L=history.length, returnList = [];
 		if (n>history.length) {return undefined}
 		for (n;n>0;n--) {returnList.push(history[L-n])}
 		return returnList.join("");
-	};
-	this.findSpell = function() {
+	},
+	findSpell : function(history) {
+		history = history || this.history;
 		var key,spell,spelling,matchList=[],result="";
 		for (key in spellbook) {
 			spell = spellbook[key];
 			spelling = spell.spell;
-			if (spelling === this.getLast(spelling.length)) {matchList.push(spell);}
+			if (spelling === this.getLast(spelling.length,history)) {matchList.push(spell);}
 		}
 		return matchList.reduce(function(prev,curr) {return ((curr.spell.length > prev.spell.length) ? curr : prev)},spellbook["null"]);
-		//for (i;i<L;i++) {if (matchList[i]["spell"].length>result.length) {result=matchList[i];}}
-	};
-	this.remember = function(c) {
+	},
+	remember : function(c) {
 		this.history.push(c);
-		while (this.history.length > 20) {this.history.shift();}
-	};
+		while (this.history.length > 7) {this.history.shift();} //don't hardcode max history length
+	},
+	anticipate : function() {
+		var options = {},history = this.history;
+		["a","b","c","d","e"].forEach(function(o) {
+			options[o] = Words.prototype.findSpell(history.concat([o]));
+		});
+		return options;
+	}
 }
 
+//___________________________Conjuration____________________________________________________________________________________________________
+
 function Conjuration(caster,rate,duration,damage,imagesrc) { //Images should be from P1's perspective
+	caster = caster || {};
 	Actor.call(this,imagesrc,caster.x,caster.y);
 	this.heading = (caster.controller===P1) ? 1 : -1;//heading;//-1 for left, 1 for right
 	this.rate = rate;
 	this.damage = damage;
 	this.caster = caster;
 	this.duration = duration;
-	this.onUpkeep = function() {
-		console.log(this.duration)
+	//if (this.heading===-1) {ctx.scale(scaleH, scaleV)}
+}
+
+Conjuration.prototype = new Actor();
+Conjuration.prototype.onUpkeep = function() {
 		this.duration -= 1;
 		if (this.duration === 0) {
 			this.isCondemned = true;
@@ -146,25 +229,30 @@ function Conjuration(caster,rate,duration,damage,imagesrc) { //Images should be 
 			return;}
 		if (this.upkeep) {this.upkeep();} 
 	};
-
-	//if (this.heading===-1) {ctx.scale(scaleH, scaleV)}
-}
+//_________________________Fireball___________________________________________________________________________________________________________
 
 function Fireball(caster) {
 	Conjuration.call(this,caster,256,1,5,"fireball");
 	this.tag = "fireball"
 }
 
+Fireball.prototype = new Conjuration();
+
+//________________________Shield_____________________________________________________________________________________________________________
+
 function Shield(caster) {
 	Conjuration.call(this,caster,0,1,0,"shield")
 	this.tag = "shield"
 	this.x -= 30;this.y -= 30;  //need to find a better solution than hardcoding. Problem is I can't refer to the width because the image is not loaded yet.
 	caster.isShielded = true;
-	this.onDestroy = function() {caster.isShielded = false};
 }
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////Spells//////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+
+Shield.prototype = new Conjuration();
+Shield.prototype.onDestroy = function() {this.caster.isShielded = false};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////Spells///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var spellbook = {
 	"null": {
@@ -172,6 +260,7 @@ var spellbook = {
 		onCast: function(caster) {}
 	},
 	fireball: {
+		name: "Fireball",
 		spell:"abc",//aabc; will use simple 1 character for testing
 		onCast: function(caster) {
 			var conj = new Fireball(caster);
@@ -179,7 +268,8 @@ var spellbook = {
 		}
 	},
 	shield: {
-		spell:"c",
+		name: "Shield",
+		spell:"b",
 		onCast: function(caster) {
 			var conj = new Shield(caster);
 			conjurations.push(conj);
@@ -187,9 +277,9 @@ var spellbook = {
 	}
 }
 
-/////////////////////////////////////////////////////////////////////////
-//////////////////////////Variable Definitions///////////////////////////
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////Variable Definitions////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var startX1 = canvas.width / 10,
 	startX2 = canvas.width * 8 / 10,
@@ -200,14 +290,12 @@ var startX1 = canvas.width / 10,
 
 	creatures = [P1,P2],
 	conjurations = [],
-	removeConjurations = [],
-	removeCreatures = [],
 	keysDown = {},
 	orders = {P1:"",P2:""};
 
-/////////////////////////////////////////////////////////////////////////
-////////////////////////////Controls Handler/////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////Controls Handler//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
@@ -217,9 +305,9 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////Game Update Functions///////////////////////////
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////Game Update Functions////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Update game objects
 var update = function (modifier) {
@@ -254,7 +342,6 @@ var reset = function() {
 	P1.x = canvas.width / 10;
 	P1.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
 	P2.x = canvas.width * 8 / 10;
 	P2.y = canvas.height / 2;
 }
@@ -272,20 +359,24 @@ var render = function () {
 
 	// Life
 	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
+	ctx.font = "50px Helvetica";
 	ctx.fillStyle = "green";
-	ctx.textBaseline = "top";
+	ctx.textBaseline = "bottom";
 
 	ctx.textAlign = "left";
-	ctx.fillText("Life: " + P1.health, 32, 400-32);
+	ctx.fillText("Life: " + P1.health, 32, canvas.height-32);
 	ctx.textAlign = "right";
-	ctx.fillText("Life: " + P2.health, 600-32, 400-32);
+	ctx.fillText("Life: " + P2.health, canvas.width-32, canvas.height-32);
 
 	// Time
+	ctx.font = "60px Helvetica";
 	ctx.fillStyle = "black";
 	ctx.textAlign = "center";
 	var text = (parseInt((nextResolve-Date.now())/1000)+1).toString();
-	ctx.fillText(text,300,200);
+	ctx.fillText(text,canvas.width/2,canvas.height/2);
+
+	P1.publishHistory();
+	P1.publishOptions();
 };
 
 //Resolve player actions between rounds
@@ -304,6 +395,7 @@ var resolve = function() {
 	var P1S = P1W.findSpell(),P2S = P2W.findSpell();
 	if (P1S) {P1S.onCast(P1)}
 	if (P2S) {P2S.onCast(P2)}
+
 	console.log(P1.words.history);
 	console.log(P2.words.history);
 }
