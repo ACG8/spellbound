183877c494aa3395741804a7c71bf45862838016
ACG <anandaguneratne@gmail.com>
2015-09-11 13:29:51 -0500

    #1 - Animations implemented!
    Animations work now. Put in a new fireball sprite to test.

--- game.js
+++ game.js
@@ -16,27 +16,130 @@ bgImage.src = "images/background.png";
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////Preloading Images//////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-var imageDict = {};
 
-function loadImages() {
-	var imageList = [
-		"player1",
-		"player2",
-		"fireball",
-		"shield"
-	],
-	i=0,L=imageList.length,image,doneLoading = false;
-	for (i;i<L;i++) {
-		image = imageList[i]
-		imageDict[image] = new Image();
-		imageDict[image].src = "images/"+image+".png";
-		if (i===L-1) {imageDict[image].onload = function() {doneLoading = true;};}
-	}
+//Note: This code taken from James Long's tutorial "Making Sprite-Based Games with Canvas" (http://jlongster.com/Making-Sprite-based-Games-with-Canvas)
+
+(function() {
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
+
+    // Load an image url or an array of image urls
+    function load(urlOrArr) {
+        if(urlOrArr instanceof Array) {
+            urlOrArr.forEach(function(url) {
+                _load(url);
+            });
+        }
+        else {
+            _load(urlOrArr);
+        }
+    }
+
+    function _load(url) {
+        if(resourceCache[url]) {
+            return resourceCache[url];
+        }
+        else {
+            var img = new Image();
+            img.onload = function() {
+                resourceCache[url] = img;
+
+                if(isReady()) {
+                    readyCallbacks.forEach(function(func) { func(); });
+                }
+            };
+            resourceCache[url] = false;
+            img.src = url;
+        }
+    }
+
+    function get(url) {
+        return resourceCache[url];
+    }
+
+    function isReady() {
+        var ready = true;
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
+               !resourceCache[k]) {
+                ready = false;
+            }
+        }
+        return ready;
+    }
+
+    function onReady(func) {
+        readyCallbacks.push(func);
+    }
+
+    window.resources = { 
+        load: load,
+        get: get,
+        onReady: onReady,
+        isReady: isReady
+    };
+})();
+
+function Sprite(url, pos, size, speed, frames, dir, once) {
+    this.pos = pos;
+    this.size = size;
+    this.speed = typeof speed === 'number' ? speed : 0;
+    this.frames = frames;
+    this._index = 0;
+    this.url = url;
+    this.dir = dir || 'horizontal';
+    this.once = once;
+};
+
+Sprite.prototype.update = function(dt) {
+    this._index += this.speed*dt;
+};
+
+Sprite.prototype.render = function(x,y) {
+    var frame;
+
+    if(this.speed > 0) {
+        var max = this.frames.length;
+        var idx = Math.floor(this._index);
+        frame = this.frames[idx % max];
+
+        if(this.once && idx >= max) {
+            this.done = true;
+            return;
+        }
+    }
+    else {
+        frame = 0;
+    }
+
+    var sx = this.pos[0],sy = this.pos[1];
+
+    if(this.dir == 'vertical') {
+        sy += frame * this.size[1];
+    }
+    else {
+        sx += frame * this.size[0];
+    }
+    ctx.drawImage(resources.get(this.url),
+                  sx, sy,
+                  this.size[0], this.size[1],
+                  x, y,
+                  this.size[0], this.size[1]);
+};
+
+//End borrowed code
+
+var spriteSpec = {
+	error 	:  	{frames:[0],				size:[50,50],		rate:0,		pos:[0,0]},
+	fireball: 	{frames:[0,1,2,3,4,5],		size:[64,64],		rate:20,	pos:[0,0]},
+	shield 	: 	{frames:[0],				size:[111,109],		rate:0,		pos:[0,0]},
+	player1	: 	{frames:[0],				size:[50,64],		rate:0,		pos:[0,0]},
+	player2	: 	{frames:[0],				size:[50,64],		rate:0,		pos:[0,0]}
 }
 
-loadImages();
-*/
+for (var key in spriteSpec) {resources.load("images/"+key+".png")}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////Game Settings////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -96,13 +199,16 @@ runeS.src = "images/SBlock.png";
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////Constructor Functions////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function Actor(imagesrc,x,y) {
-	this.image = new Image();
-	if (imagesrc) {this.image.src = "images/"+imagesrc+".png"};
-	//this.image = imageDict[imagesrc];
-	this.x = x;
-	this.y = y;
+//function Sprite(url, pos, size, speed, frames, dir, once)
+function Actor(sprite,pos) {
+	sprite = sprite || "error"
+	pos = pos || [0,0]
+	var s = spriteSpec[sprite];
+	this.x = pos[0];
+	this.y = pos[1];
+	this.w = s.size[0];
+	this.h = s.size[1];
+	this.sprite = new Sprite("images/"+sprite+".png", s.pos, s.size, s.rate, s.frames);
 	this.isCondemned = false;
 }
 
@@ -110,17 +216,17 @@ Actor.prototype = {
 	draw : function() {ctx.drawImage(this.image,this.x,this.y)},
 	isTouching : function(other) {
 		var L1=this.x,
-			R1=L1+this.image.width,
+			R1=L1+this.w,
 			U1=this.y,
-			D1=U1+this.image.height,
+			D1=U1+this.h,
 			L2=other.x,
-			R2=L2+other.image.width,
+			R2=L2+other.w,
 			U2=other.y,
-			D2=U2+other.image.height;
+			D2=U2+other.h;
 		return (!(L1>R2 || L2>R1 || U1>D2 || U2>D1));
 	},
 	isOffscreen : function() {
-		var L=this.x,R=L+this.image.width,U=this.y,D=U+this.image.height;
+		var L=this.x,R=L+this.w,U=this.y,D=U+this.h;
 		return (L<0 || canvas.width<R || U<0 || canvas.height<D);
 	},
 	getTouching : function() {
@@ -131,8 +237,8 @@ Actor.prototype = {
 
 //_____________________________Creature_________________________________________________________________________________________________________
 
-function Creature(level,health,speed,controller,imagesrc,x,y) {
-	Actor.call(this,imagesrc);
+function Creature(level,health,speed,controller,sprite,position) {
+	Actor.call(this,sprite,position);
 	this.level = level;
 	this.health = health;
 	this.speed = speed;
@@ -143,12 +249,13 @@ Creature.prototype = new Actor();
 
 //_____________________________Wizard__________________________________________________________________________________________________________
 
-function Wizard(level,health,speed,imagesrc,x,y) {
-	Creature.call(this,level,health,speed,this,imagesrc,x,y);
+function Wizard(level,health,speed,sprite,postion) {
+	Creature.call(this,level,health,speed,this,sprite,postion);
 	this.words = new Words();
 }
 
 Wizard.prototype = new Creature();
+
 Wizard.prototype.publishHistory = function() {
 		var reversedHistory = this.words.history.slice(0);
 		reversedHistory.reverse();
@@ -156,6 +263,7 @@ Wizard.prototype.publishHistory = function() {
 			ctx.drawImage(runeBindings["P1"][o],canvas.width/2-100-i*50,0);
 		})
 	};
+
 Wizard.prototype.publishOptions = function() {
 		var options = this.words.anticipate(),key,i=0;
 		for (key in options) {
@@ -209,14 +317,16 @@ Words.prototype = {
 
 //___________________________Conjuration____________________________________________________________________________________________________
 
-function Conjuration(caster,rate,duration,damage,imagesrc) { //Images should be from P1's perspective
+function Conjuration(caster,speed,duration,damage,sprite) { //Images should be from P1's perspective
+	this.tag = sprite;
 	caster = caster || {};
-	Actor.call(this,imagesrc,caster.x,caster.y);
+	Actor.call(this,sprite,[caster.x,caster.y]);
 	this.heading = (caster.controller===P1) ? 1 : -1;//heading;//-1 for left, 1 for right
-	this.rate = rate;
+	this.speed = speed;
 	this.damage = damage;
 	this.caster = caster;
 	this.duration = duration;
+
 	//if (this.heading===-1) {ctx.scale(scaleH, scaleV)}
 }
 
@@ -233,7 +343,6 @@ Conjuration.prototype.onUpkeep = function() {
 
 function Fireball(caster) {
 	Conjuration.call(this,caster,256,1,5,"fireball");
-	this.tag = "fireball"
 }
 
 Fireball.prototype = new Conjuration();
@@ -242,7 +351,6 @@ Fireball.prototype = new Conjuration();
 
 function Shield(caster) {
 	Conjuration.call(this,caster,0,1,0,"shield")
-	this.tag = "shield"
 	this.x -= 30;this.y -= 30;  //need to find a better solution than hardcoding. Problem is I can't refer to the width because the image is not loaded yet.
 	caster.isShielded = true;
 }
@@ -261,7 +369,7 @@ var spellbook = {
 	},
 	fireball: {
 		name: "Fireball",
-		spell:"abc",//aabc; will use simple 1 character for testing
+		spell:"c",//aabc; will use simple 1 character for testing
 		onCast: function(caster) {
 			var conj = new Fireball(caster);
 			conjurations.push(conj);
@@ -280,13 +388,13 @@ var spellbook = {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////Variable Definitions////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//Wizard(level,health,speed,sprite,postion)
 var startX1 = canvas.width / 10,
 	startX2 = canvas.width * 8 / 10,
 	startY = canvas.height / 2,
 
-	P1 = new Wizard(7,20,4,"player1",startX1,startY),
-	P2 = new Wizard(7,20,4,"player2",startX2,startY),
+	P1 = new Wizard(7,20,4,"player1",[startX1,startY]),
+	P2 = new Wizard(7,20,4,"player2",[startX2,startY]),
 
 	creatures = [P1,P2],
 	conjurations = [],
@@ -324,7 +432,8 @@ var update = function (modifier) {
 	var enemy;
 	conjurations.forEach(function(o,i) {
 		enemy = ((o.caster===P1) ? P2 : P1);
-		o.x += o.rate * o.heading * modifier;
+		o.x += o.speed * o.heading * modifier;
+		o.sprite.update(modifier);
 		if (o.isTouching(enemy)) {
 			if (!enemy.isShielded) {enemy.health -= o.damage}
 			if (o.onDestroy) {o.onDestroy();}
@@ -351,11 +460,13 @@ var render = function () {
 	//Draw background
 	if (bgReady) {ctx.drawImage(bgImage, 0, 0);}
 	//Draw creatures
-	var i=0,L=creatures.length;
-	for (i;i<L;i++) {creatures[i].draw();}
+	var i=0,L=creatures.length,subject;
+	for (i;i<L;i++) {	subject=creatures[i];
+						subject.sprite.render(subject.x,subject.y);}
 	//Draw conjurations
 	L=conjurations.length;
-	for (i=0;i<L;i++) {conjurations[i].draw();}
+	for (i=0;i<L;i++) {	subject=conjurations[i];
+						subject.sprite.render(subject.x,subject.y);}
 
 	// Life
 	ctx.fillStyle = "rgb(250, 250, 250)";
@@ -396,8 +507,8 @@ var resolve = function() {
 	if (P1S) {P1S.onCast(P1)}
 	if (P2S) {P2S.onCast(P2)}
 
-	console.log(P1.words.history);
-	console.log(P2.words.history);
+	//console.log(P1.words.history);
+	//console.log(P2.words.history);
 }
 
 // The main game loop
@@ -415,7 +526,7 @@ var main = function () {
 
 	then = now;
 
-	// Request to do this again ASAP
+	//Recursion
 	requestAnimationFrame(main);
 };
 
@@ -427,4 +538,7 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 var nextResolve = Date.now() + roundTime;
 var then = Date.now();
 reset();
-main();
+
+resources.onReady(main);
+
+//main();
